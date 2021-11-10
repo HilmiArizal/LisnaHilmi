c377a2f5604fb886a1aa0c8eeff90ddfb88c851b
Hilmi Arizal <hilmi.arizal36@gmail.com>
2021-11-10 13:21:51 +0700

    update-url-server

--- src/Service/API_URL.js
+++ src/Service/API_URL.js
@@ -1,2 +1,2 @@
 export const API_URL = "https://lisnahilmi.herokuapp.com/api/";
-export const API_URL_IMAGE = "http://localhost:8000/";
+export const API_URL_IMAGE = "https://lisnahilmi.herokuapp.com/";
